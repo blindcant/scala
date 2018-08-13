6334d32e455f3e2e41aac12f1302b047ead56175
blindcant <blindcant@gmail.com>
2018-08-14 08:47:05 +1000

    Command line argument processing using imperative loops.

--- Programming_In_Scala_3E/src/chapter02_getting_started/CommandLineArgumentsImperative.scala
+++ Programming_In_Scala_3E/src/chapter02_getting_started/CommandLineArgumentsImperative.scala
@@ -1,6 +1,6 @@
 package chapter02_getting_started
 
-object CommandLineArguments extends App {
+object CommandLineArgumentsImperative extends App {
 	var i = 0
 	while (i < args.length) {
 		println("args(" + i + ") is " + args(i))
