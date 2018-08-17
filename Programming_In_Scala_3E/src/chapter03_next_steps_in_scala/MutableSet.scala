df7c2ac22e64926bfdf595af4dfa09ca9f5835dc
blindcant <blindcant@gmail.com>
2018-08-17 15:47:15 +1000

    Fixed an incorrect comment about the += method.

--- Programming_In_Scala_3E/src/chapter03_next_steps_in_scala/MutableSet.scala
+++ Programming_In_Scala_3E/src/chapter03_next_steps_in_scala/MutableSet.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable
 
 object MutableSet extends App {
 	val movieSet =  mutable.Set("Hitch", "Poltergeist")
-	// Shorthand for movieSet = movieSet + "Shrek"
+	// +=  is actually a method for mutable sets.
 	movieSet += "Shrek"
 	println(movieSet)
 }
