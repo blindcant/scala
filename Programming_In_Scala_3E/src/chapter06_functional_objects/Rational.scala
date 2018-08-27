0a383d87e10d2cfeac9e5bd3bafb96d2ea7a4b66
blindcant <blindcant@gmail.com>
2018-08-27 15:32:24 +1000

    Now simplifies the result.

--- Programming_In_Scala_3E/src/chapter06_functional_objects/Rational.scala
+++ Programming_In_Scala_3E/src/chapter06_functional_objects/Rational.scala
@@ -7,8 +7,14 @@ class Rational(n: Int, d: Int) {
 	// A primary constructor precondition that places constraints on the constructor's parameters
 	require(d != 0)
 
+	private val greatestCommonDivisor = gcd(n.abs, d.abs)
 	val numerator: Int = n
 	val denominator: Int = d
+
+	// An auxillary constructor that is chained to the primary constructor
+	def this(n: Int) = {
+		this(n, 1)
+	}
 
 	// Over riding Java's Object.toString
 	override def toString = numerator + "/" + denominator
@@ -22,10 +28,23 @@ class Rational(n: Int, d: Int) {
 			denominator * rationalObject.denominator
 		)
 	}
+
+	def lessThan(that:Rational) = {
+		this.numerator * that.denominator < that.numerator * this.denominator
+	}
+
+	private def gcd(a: Int, b: Int): Int = {
+		if (b == 0) {
+			a
+		}
+		else {
+			gcd(b, a % b)
+		}
+	}
 }
 
 object Rational extends App {
-	val oneHalf = new Rational(1, 2)
+	val oneHalf = new Rational(1)
 	println(oneHalf.toString)
 
 	val twoThirds = new Rational(1, 2)
