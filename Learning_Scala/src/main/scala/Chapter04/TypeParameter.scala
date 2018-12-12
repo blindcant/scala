0cddd9a14caf02e55110b4c9675cc374ee6d7451
blindcant <blindcant@gmail.com>
2018-12-12 15:22:33 +1100

    Type parametising.

---
+++ Learning_Scala/src/main/scala/Chapter04/TypeParameter.scala
@@ -0,0 +1,26 @@
+/*
+An identifier function simply returns whatever is passed to it.
+The T (type) is defining the type as a parameter which is set by the caller. The value of this is that the return type
+will be the same as what was passed in.
+*/
+
+def identifier[T](input: T): T = input
+
+val result = identifier[String]("Hi")
+println(result)
+println(result.getClass)
+
+val result2 = identifier[Int](8)
+println(result2)
+println(result2.getClass)
+
+// The return type here will be Any, not what was passed. Run this in REPL to see.
+def identifierAny(input: Any): Any = input
+
+val result3 = identifierAny("Hi")
+println(result3)
+println(result3.getClass)
+
+val result4 = identifierAny(8)
+println(result4)
+println(result4.getClass)
