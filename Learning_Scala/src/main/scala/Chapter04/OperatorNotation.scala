bd9d33a8ab6277f037f24e99c61f05ef10925b78
blindcant <blindcant@gmail.com>
2018-12-12 15:38:25 +1100

    Infix operator notation v dot notation method calls.

---
+++ Learning_Scala/src/main/scala/Chapter04/OperatorNotation.scala
@@ -0,0 +1,13 @@
+/*
+Scala has no numeric operators, they are actually methods for each type. They can be typed 2 ways, like a normal
+arithmetic call with or with the dot notation method calls.
+ */
+
+val pi = Math.PI
+
+// Dot notation
+println(pi.*(2))
+println(pi./(2))
+// Infix operator notation
+println(pi + 2)
+println(pi - 2)
